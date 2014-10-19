bb916c85db39bed84da9ab5f5825063fe4161721
Steven John Miller <steven.jmiller@gmail.com>
2014-10-19 19:20:56 +1100

    Filter years

--- public/js/controllers/SearchController.js
+++ public/js/controllers/SearchController.js
@@ -1,12 +1,18 @@
-SearchController.$inject = ['$scope', '$http', '$interval', 'earthquakes', 'ResolutionService'];
+SearchController.$inject = ['$scope', '$http', '$interval', '$routeParams', 'earthquakes', 'ResolutionService'];
 
-function SearchController($scope, $http, $interval, earthquakes, resolutionService) {
+function SearchController($scope, $http, $interval, $routeParams, earthquakes, resolutionService) {
 
-  console.log(resolutionService);
+  console.log($routeParams.year);
 
   $scope.resolutionGrid = resolutionService.largeResolution ? 3 : 2;
+  $scope.earthquakeYear = $routeParams.year;
 
   $scope.earthquakes = earthquakes;
+
+  $scope.matchYear = function() {
+
+    return this.key.match($scope.earthquakeYear) ? true : false;
+  };
 
   $http.get('/api/totals').then(
     function success(response) {
