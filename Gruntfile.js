385207f81bfbab2d3cf84f69502b6ad2dc0c32eb
Steven John Miller <steven.jmiller@gmail.com>
2014-10-22 16:09:53 +1100

    Grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,5 +83,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['less', 'develop', 'open', 'watch']);
+  grunt.registerTask('build',   ['less']);
 
 };
